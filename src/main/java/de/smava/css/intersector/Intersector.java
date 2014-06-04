ba041d4bba2dee3e7f71095a6d7498767af7f519
Askar Akhmerov <aakhmerov@ostdlabs.com>
2014-06-04 12:39:27 +0200

    1. format code 2. optimize imports

--- src/main/java/de/smava/css/intersector/Intersector.java
+++ src/main/java/de/smava/css/intersector/Intersector.java
@@ -1,125 +1,115 @@
 package de.smava.css.intersector;
 
+import com.phloc.css.ECSSVersion;
+import com.phloc.css.ICSSWriterSettings;
+import com.phloc.css.decl.*;
+import com.phloc.css.reader.CSSReader;
+import com.phloc.css.writer.CSSWriter;
+import com.phloc.css.writer.CSSWriterSettings;
+import de.smava.css.intersector.comparators.CSSDeclarationsComparator;
+import de.smava.css.intersector.comparators.CSSSelectorsComparator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.charset.Charset;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.phloc.css.ECSSVersion;
-import com.phloc.css.ICSSWriterSettings;
-import com.phloc.css.decl.CSSDeclaration;
-import com.phloc.css.decl.CSSSelector;
-import com.phloc.css.decl.CSSStyleRule;
-import com.phloc.css.decl.CascadingStyleSheet;
-import com.phloc.css.decl.ICSSTopLevelRule;
-import com.phloc.css.reader.CSSReader;
-import com.phloc.css.writer.CSSWriter;
-import com.phloc.css.writer.CSSWriterSettings;
-
-import de.smava.css.intersector.comparators.CSSDeclarationsComparator;
-import de.smava.css.intersector.comparators.CSSSelectorsComparator;
-import de.smava.css.intersector.exceptions.CSSSelectorMultipleAppearanceException;
-import de.smava.css.stripper.Main;
-
 /**
  * @author Daniel Suszczynski
- *
  */
 
 public class Intersector {
 
-	private static final String CHARSET = "UTF-8";
+    private static final String CHARSET = "UTF-8";
     private static final Logger LOGGER = LoggerFactory.getLogger(Intersector.class);
     private static final ECSSVersion CSS_VERSION = ECSSVersion.CSS30;
-    
+
     /**
      * Intersect two CSS styles sets
-     * @param String toIntersectA CSS content to intersect
-     * @param String toIntersectB CSS content to intersect
-     * @return Map<String key, StringBuffer CSScode> keys: intersected, differenceA, differenceB 
+     *
+     * @param toIntersectA CSS content to intersect
+     * @param toIntersectB CSS content to intersect
+     * @return Map<String key, StringBuffer CSScode> keys: intersected, differenceA, differenceB
      */
     public Map<IntersectDataType, StringBuffer> intersect(String toIntersectA, String toIntersectB) {
-    	Map<IntersectDataType, StringBuffer> result =  new HashMap<IntersectDataType, StringBuffer>();
-    	
-    	StringBuffer intersected = new StringBuffer();
-    	StringBuffer differenceA = new StringBuffer();
-    	StringBuffer differenceB = new StringBuffer();
-    	
+        Map<IntersectDataType, StringBuffer> result = new HashMap<IntersectDataType, StringBuffer>();
+
+        StringBuffer intersected = new StringBuffer();
+        StringBuffer differenceA = new StringBuffer();
+        StringBuffer differenceB = new StringBuffer();
+
         CascadingStyleSheet cssToIntersectA = CSSReader.readFromString(toIntersectA, Charset.forName(CHARSET), CSS_VERSION);
         CascadingStyleSheet cssToIntersectB = CSSReader.readFromString(toIntersectB, Charset.forName(CHARSET), CSS_VERSION);
 //        List<String> analyzedSelectors = listData(analyzedData);
-        
+
         CascadingStyleSheet intersectedCSS = new CascadingStyleSheet();
         CascadingStyleSheet differenceACSS = new CascadingStyleSheet();
         CascadingStyleSheet differenceBCSS = new CascadingStyleSheet();
 
         List<ICSSTopLevelRule> allRulesA = cssToIntersectA.getAllRules();
 //        List<ICSSTopLevelRule> allRulesB = cssToIntersectB.getAllRules();
-        
+
         CSSSelectorsComparator cssSelectorComparator = new CSSSelectorsComparator();
         CSSDeclarationsComparator cssDeclarationsComparator = new CSSDeclarationsComparator();
-        
+
         System.out.println("selectorsA:");
         for (ICSSTopLevelRule ruleCSSA : allRulesA) {
-        	int selectorAppearanceInRuleCSSB = 0;
-        	
-        	for(ICSSTopLevelRule ruleCSSB : cssToIntersectB.getAllRules()) {
-        		// search for adequate selector in B
-        		if(cssSelectorComparator.compare(ruleCSSA, ruleCSSB) == 0) {
-        			selectorAppearanceInRuleCSSB++;
-        			
-        			// if selector appears more than once in B, then throw exception (developer should check CSS, and make improvements there)
-        			// therefore there is no break; in the end of this method
-        			/*if(selectorAppearanceInRuleCSSB > 1) {
-        				throw new CSSSelectorMultipleAppearanceException("Multiple Appearance of CSSSelector : " + ((CSSStyleRule) ruleCSSB).getSelectorsAsCSSString(new CSSWriterSettings(CSS_VERSION), 0));
+            int selectorAppearanceInRuleCSSB = 0;
+
+            for (ICSSTopLevelRule ruleCSSB : cssToIntersectB.getAllRules()) {
+                // search for adequate selector in B
+                if (cssSelectorComparator.compare(ruleCSSA, ruleCSSB) == 0) {
+                    selectorAppearanceInRuleCSSB++;
+
+                    // if selector appears more than once in B, then throw exception (developer should check CSS, and make improvements there)
+                    // therefore there is no break; in the end of this method
+                    /*if(selectorAppearanceInRuleCSSB > 1) {
+                        throw new CSSSelectorMultipleAppearanceException("Multiple Appearance of CSSSelector : " + ((CSSStyleRule) ruleCSSB).getSelectorsAsCSSString(new CSSWriterSettings(CSS_VERSION), 0));
         			}*/
-        			
-        			// compare rule declarations
-        			if(cssDeclarationsComparator.compare(ruleCSSA, ruleCSSB) == 0) {
-        				intersectedCSS.addRule(ruleCSSA);
-        			}
-        			// otherwise pull out common declarations and placed them to intersected/${file_name}_intersected.css,
-        			// rest of declarations put accordingly to differences_a/${file_name}.css and differences_b/${file_name}.css
-        			else {
-        				Map<IntersectDataType, ICSSTopLevelRule> cssDeclarationsMerge = this.cssDeclarationsMerge(ruleCSSA, ruleCSSB);
-        				intersectedCSS.addRule(cssDeclarationsMerge.get(IntersectDataType.INTERSECTED));
-        				differenceACSS.addRule(cssDeclarationsMerge.get(IntersectDataType.DIFFERENCE_A));
-        				differenceBCSS.addRule(cssDeclarationsMerge.get(IntersectDataType.DIFFERENCE_B));
-        			}
-        			
-        			// remove rule A and B from lists
-        			cssToIntersectA.removeRule(ruleCSSA);
-        			cssToIntersectB.removeRule(ruleCSSB);
-        			
-        			break;
-        		}
-        	}
-        	
-        	// if selector from CSS A didn't appear in CSS B, then move it to differences_a/${file_name}.css
-        	if(selectorAppearanceInRuleCSSB == 0) {
-        		differenceACSS.addRule(ruleCSSA);
-        	}
-        	
-        	
-        	// TODO delete displaying part
-        	// displaying
-        	for(CSSSelector cssSelectorA : ((CSSStyleRule) ruleCSSA).getAllSelectors()) {
-        		System.out.println("\t" + cssSelectorA.getAsCSSString(new CSSWriterSettings(CSS_VERSION), 0));
-        	}
-        	
+
+                    // compare rule declarations
+                    if (cssDeclarationsComparator.compare(ruleCSSA, ruleCSSB) == 0) {
+                        intersectedCSS.addRule(ruleCSSA);
+                    }
+                    // otherwise pull out common declarations and placed them to intersected/${file_name}_intersected.css,
+                    // rest of declarations put accordingly to differences_a/${file_name}.css and differences_b/${file_name}.css
+                    else {
+                        Map<IntersectDataType, ICSSTopLevelRule> cssDeclarationsMerge = this.cssDeclarationsMerge(ruleCSSA, ruleCSSB);
+                        intersectedCSS.addRule(cssDeclarationsMerge.get(IntersectDataType.INTERSECTED));
+                        differenceACSS.addRule(cssDeclarationsMerge.get(IntersectDataType.DIFFERENCE_A));
+                        differenceBCSS.addRule(cssDeclarationsMerge.get(IntersectDataType.DIFFERENCE_B));
+                    }
+
+                    // remove rule A and B from lists
+                    cssToIntersectA.removeRule(ruleCSSA);
+                    cssToIntersectB.removeRule(ruleCSSB);
+
+                    break;
+                }
+            }
+
+            // if selector from CSS A didn't appear in CSS B, then move it to differences_a/${file_name}.css
+            if (selectorAppearanceInRuleCSSB == 0) {
+                differenceACSS.addRule(ruleCSSA);
+            }
+
+
+            // TODO delete displaying part
+            // displaying
+            for (CSSSelector cssSelectorA : ((CSSStyleRule) ruleCSSA).getAllSelectors()) {
+                System.out.println("\t" + cssSelectorA.getAsCSSString(new CSSWriterSettings(CSS_VERSION), 0));
+            }
+
 //            String selectorsA = ((CSSStyleRule) rule).getSelectorsAsCSSString(new CSSWriterSettings(CSS_VERSION), 0);
 //            System.out.println("\t" + selectorsA);
-            if(((CSSStyleRule) ruleCSSA).getAllDeclarations() != null && ((CSSStyleRule) ruleCSSA).getAllDeclarations().size() > 0) {
-            	for(CSSDeclaration cssDeclarationA : ((CSSStyleRule) ruleCSSA).getAllDeclarations()) {
-            		System.out.println("\t\t" + cssDeclarationA.getAsCSSString(new CSSWriterSettings(CSS_VERSION), 0) /*+ " : " + cssDeclarationA.getExpression().getAsCSSString(new CSSWriterSettings(CSS_VERSION), 0)*/);
-            	}
-            }
-            else {
-            	System.out.println("\t\t-");
+            if (((CSSStyleRule) ruleCSSA).getAllDeclarations() != null && ((CSSStyleRule) ruleCSSA).getAllDeclarations().size() > 0) {
+                for (CSSDeclaration cssDeclarationA : ((CSSStyleRule) ruleCSSA).getAllDeclarations()) {
+                    System.out.println("\t\t" + cssDeclarationA.getAsCSSString(new CSSWriterSettings(CSS_VERSION), 0) /*+ " : " + cssDeclarationA.getExpression().getAsCSSString(new CSSWriterSettings(CSS_VERSION), 0)*/);
+                }
+            } else {
+                System.out.println("\t\t-");
             }
 //          replace all whitespaces and chars like \t, \n from selector signature
            /* String clean = original.replaceAll("\\s+","");
@@ -128,104 +118,105 @@ public class Intersector {
             }*/
             System.out.println("");
         }
-        
+
         // move all rules which left in CSS B to differences_b/${file_name}.css
         for (ICSSTopLevelRule leftRuleCSSB : cssToIntersectB.getAllRules()) {
-        	differenceBCSS.addRule(leftRuleCSSB);
-    	}
-        
+            differenceBCSS.addRule(leftRuleCSSB);
+        }
+
         CSSWriter cssIntersectedWriter = new CSSWriter(CSS_VERSION);
         try {
-        	cssIntersectedWriter.setHeaderText("");
-        	intersected.append(cssIntersectedWriter.getCSSAsString(intersectedCSS));
+            cssIntersectedWriter.setHeaderText("");
+            intersected.append(cssIntersectedWriter.getCSSAsString(intersectedCSS));
         } catch (Exception e) {
-        	LOGGER.error("Can't write resulting intersectedCSS CSS to intersected!", e);
+            LOGGER.error("Can't write resulting intersectedCSS CSS to intersected!", e);
         }
-        
+
         CSSWriter cssDifferenceAWriter = new CSSWriter(CSS_VERSION);
         try {
-        	cssDifferenceAWriter.setHeaderText("");
-        	differenceA.append(cssDifferenceAWriter.getCSSAsString(differenceACSS));
+            cssDifferenceAWriter.setHeaderText("");
+            differenceA.append(cssDifferenceAWriter.getCSSAsString(differenceACSS));
         } catch (Exception e) {
-        	LOGGER.error("Can't write resulting differenceACSS CSS to differenceA!", e);
+            LOGGER.error("Can't write resulting differenceACSS CSS to differenceA!", e);
         }
-        
+
         CSSWriter cssDifferenceBWriter = new CSSWriter(CSS_VERSION);
         try {
-        	cssDifferenceBWriter.setHeaderText("");
-        	differenceB.append(cssDifferenceBWriter.getCSSAsString(differenceBCSS));
+            cssDifferenceBWriter.setHeaderText("");
+            differenceB.append(cssDifferenceBWriter.getCSSAsString(differenceBCSS));
         } catch (Exception e) {
-        	LOGGER.error("Can't write resulting differenceBCSS CSS to differenceB!", e);
+            LOGGER.error("Can't write resulting differenceBCSS CSS to differenceB!", e);
         }
-        
+
         result.put(IntersectDataType.INTERSECTED, intersected);
         result.put(IntersectDataType.DIFFERENCE_A, differenceA);
         result.put(IntersectDataType.DIFFERENCE_B, differenceB);
-    	return result;
-	}
-    
+        return result;
+    }
+
     /**
      * Compare and merge declaration form given rule. Declaration are spited to three types: intersected, differenceA, differenceB
-     * @param ICSSTopLevelRule icssTopLevelRuleA rule A to compare and merge
-     * @param ICSSTopLevelRule icssTopLevelRuleB rule B to compare and merge
-     * @return Map<IntersectDataType key, ICSSTopLevelRule rule> keys: intersected, differenceA, differenceB 
+     *
+     * @param icssTopLevelRuleA rule A to compare and merge
+     * @param icssTopLevelRuleB rule B to compare and merge
+     * @return Map<IntersectDataType key, ICSSTopLevelRule rule> keys: intersected, differenceA, differenceB
      */
     private Map<IntersectDataType, ICSSTopLevelRule> cssDeclarationsMerge(ICSSTopLevelRule icssTopLevelRuleA, ICSSTopLevelRule icssTopLevelRuleB) {
-    	Map<IntersectDataType, ICSSTopLevelRule> result =  new HashMap<IntersectDataType, ICSSTopLevelRule>();
-    	
-    	CSSStyleRule intersectedRule = new CSSStyleRule();
-    	CSSStyleRule differenceRuleA = new CSSStyleRule();
-    	CSSStyleRule differenceRuleB = new CSSStyleRule();
-    	
-    	ICSSWriterSettings settings = new CSSWriterSettings(CSS_VERSION);
-    	
-    	if (icssTopLevelRuleA instanceof CSSStyleRule && icssTopLevelRuleB instanceof CSSStyleRule) {
+        Map<IntersectDataType, ICSSTopLevelRule> result = new HashMap<IntersectDataType, ICSSTopLevelRule>();
+
+        CSSStyleRule intersectedRule = new CSSStyleRule();
+        CSSStyleRule differenceRuleA = new CSSStyleRule();
+        CSSStyleRule differenceRuleB = new CSSStyleRule();
+
+        ICSSWriterSettings settings = new CSSWriterSettings(CSS_VERSION);
+
+        if (icssTopLevelRuleA instanceof CSSStyleRule && icssTopLevelRuleB instanceof CSSStyleRule) {
             CSSStyleRule styleRuleA = (CSSStyleRule) icssTopLevelRuleA;
             CSSStyleRule styleRuleB = (CSSStyleRule) icssTopLevelRuleB;
-            
+
             // add selectors to new rules
             for (CSSSelector cssSelector : styleRuleA.getAllSelectors()) {
-            	intersectedRule.addSelector(cssSelector);
-            	differenceRuleA.addSelector(cssSelector);
-            	differenceRuleB.addSelector(cssSelector);
-			}
-            
-            for(CSSDeclaration cssDeclarationA : styleRuleA.getAllDeclarations()) {
-    			boolean declarationAFoundInRuleB = false;
-    			
-    			// check if current declaration from rule A is in rule B
-    			for(CSSDeclaration cssDeclarationB : styleRuleB.getAllDeclarations()) {
-    				if(cssDeclarationB.getAsCSSString(settings,0).equals(cssDeclarationA.getAsCSSString(settings,0))) {
-    					declarationAFoundInRuleB = true;
-    					
-    					// when declaration from rule A is in rule B, then put it to intersected
-    					intersectedRule.addDeclaration(cssDeclarationA);
-        				styleRuleA.removeDeclaration(cssDeclarationA);
-        				styleRuleB.removeDeclaration(cssDeclarationB);
-    					
-    					break;
-    				}
-    			}
-    			
-    			// when declaration from rule A is not in rule B, then remove it only from rule A
-    			// declarations from rule B which ware not found, will be moved to differenceB
-    			if(!declarationAFoundInRuleB) {
-    				differenceRuleA.addDeclaration(cssDeclarationA);
-    			}
-    		}
-            
-            // move others declarations from B to differenceB
-            if(styleRuleB.getAllDeclarations().size() > 0) {
-            	for(CSSDeclaration cssDeclarationB : styleRuleB.getAllDeclarations()) {
-            		differenceRuleB.addDeclaration(cssDeclarationB);
-            	}
+                intersectedRule.addSelector(cssSelector);
+                differenceRuleA.addSelector(cssSelector);
+                differenceRuleB.addSelector(cssSelector);
             }
-    	}
-    	
-    	result.put(IntersectDataType.INTERSECTED, intersectedRule);
+
+            for (CSSDeclaration cssDeclarationA : styleRuleA.getAllDeclarations()) {
+                boolean declarationAFoundInRuleB = false;
+
+                // check if current declaration from rule A is in rule B
+                for (CSSDeclaration cssDeclarationB : styleRuleB.getAllDeclarations()) {
+                    if (cssDeclarationB.getAsCSSString(settings, 0).equals(cssDeclarationA.getAsCSSString(settings, 0))) {
+                        declarationAFoundInRuleB = true;
+
+                        // when declaration from rule A is in rule B, then put it to intersected
+                        intersectedRule.addDeclaration(cssDeclarationA);
+                        styleRuleA.removeDeclaration(cssDeclarationA);
+                        styleRuleB.removeDeclaration(cssDeclarationB);
+
+                        break;
+                    }
+                }
+
+                // when declaration from rule A is not in rule B, then remove it only from rule A
+                // declarations from rule B which ware not found, will be moved to differenceB
+                if (!declarationAFoundInRuleB) {
+                    differenceRuleA.addDeclaration(cssDeclarationA);
+                }
+            }
+
+            // move others declarations from B to differenceB
+            if (styleRuleB.getAllDeclarations().size() > 0) {
+                for (CSSDeclaration cssDeclarationB : styleRuleB.getAllDeclarations()) {
+                    differenceRuleB.addDeclaration(cssDeclarationB);
+                }
+            }
+        }
+
+        result.put(IntersectDataType.INTERSECTED, intersectedRule);
         result.put(IntersectDataType.DIFFERENCE_A, differenceRuleA);
         result.put(IntersectDataType.DIFFERENCE_B, differenceRuleB);
-    	return result;
+        return result;
     }
-    
+
 }
--- src/main/java/de/smava/css/intersector/Main.java
+++ src/main/java/de/smava/css/intersector/Main.java
@@ -1,27 +1,24 @@
 package de.smava.css.intersector;
 
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import de.smava.css.intersector.exceptions.CSSSelectorMultipleAppearanceException;
-import de.smava.css.stripper.Stripper;
-
 /**
  * Intersect takes two sets (folders): oryginals_a, originals_b of CSS files and intersect them. As a result produce three folders with CSS files: intersected, differences_a, differences_b.
  * Remember that all files from set_a must have theirs equivalent in name in set_b.
- * @author Daniel Suszczynski
  *
+ * @author Daniel Suszczynski
  */
 
 public class Main {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     private static final String ORIGINALS_A_SUB_PATH = "/originals_a";
     private static final String ORIGINALS_B_SUB_PATH = "/originals_b";
     private static final String INTERSECTED_SUB_PATH = "/intersected";
@@ -29,12 +26,11 @@ public class Main {
     private static final String DIFFERENCES_B_SUB_PATH = "/differences_b";
     private static final String INTERSECTED_OUT_SUFFIX = "_intersected.css";
     private static final String DEFAULT_CHARSET = "UTF-8";
-    
-	public static void main(String[] args) {
-		if (args.length != 1 || args[0] == null) {
+
+    public static void main(String[] args) {
+        if (args.length != 1 || args[0] == null) {
             printInfo();
-        } 
-		else {
+        } else {
             String basePath = args[0];
             File originalsAFolder = new File(basePath + ORIGINALS_A_SUB_PATH);
             File originalsBFolder = new File(basePath + ORIGINALS_B_SUB_PATH);
@@ -44,22 +40,22 @@ public class Main {
 
 //          clean outgoing files if they exists
             if (intersectedFolder.exists()) {
-            	intersectedFolder.delete();
+                intersectedFolder.delete();
             }
             intersectedFolder.mkdirs();
-            
+
             if (differencesAFolder.exists()) {
-            	differencesAFolder.delete();
+                differencesAFolder.delete();
             }
             differencesAFolder.mkdirs();
-            
+
             if (differencesBFolder.exists()) {
-            	differencesBFolder.delete();
+                differencesBFolder.delete();
             }
             differencesBFolder.mkdirs();
-            
+
             Intersector intersect = new Intersector();
-            
+
             for (final File originalAFolderFile : originalsAFolder.listFiles()) {
                 FilenameFilter originalsABFoldersFilenameFilter = new FilenameFilter() {
                     public boolean accept(File dir, String name) {
@@ -73,52 +69,51 @@ public class Main {
                 File[] matchingOriginalBFolderFiles = originalsBFolder.listFiles(originalsABFoldersFilenameFilter);
 
                 if (matchingOriginalBFolderFiles == null || matchingOriginalBFolderFiles.length != 1) {
-                	LOGGER.error("Matching oryginals A files should be mappable 1 to 1 with oryginals B");
-                	LOGGER.error("Current matches : [" + matchingOriginalBFolderFiles + "]");
-                }
-                else {
+                    LOGGER.error("Matching oryginals A files should be mappable 1 to 1 with oryginals B");
+                    LOGGER.error("Current matches : [" + matchingOriginalBFolderFiles + "]");
+                } else {
                     File originalBFolderFile = matchingOriginalBFolderFiles[0];
-                    File intersectedFolderFile = new File (intersectedFolder.getAbsolutePath() + "/" + originalAFolderFile.getName().split("\\.")[0] + INTERSECTED_OUT_SUFFIX);
-                    File differencesAFolderFile = new File (differencesAFolder.getAbsolutePath() + "/" + originalAFolderFile.getName());
-                    File differencesBFolderFile = new File (differencesBFolder.getAbsolutePath() + "/" + originalAFolderFile.getName());
-                    
+                    File intersectedFolderFile = new File(intersectedFolder.getAbsolutePath() + "/" + originalAFolderFile.getName().split("\\.")[0] + INTERSECTED_OUT_SUFFIX);
+                    File differencesAFolderFile = new File(differencesAFolder.getAbsolutePath() + "/" + originalAFolderFile.getName());
+                    File differencesBFolderFile = new File(differencesBFolder.getAbsolutePath() + "/" + originalAFolderFile.getName());
+
                     Map<IntersectDataType, StringBuffer> intersectedData = null;
-                    
+
                     try {
-                    	intersectedData = intersect.intersect(FileUtils.readFileToString(originalAFolderFile),FileUtils.readFileToString(originalBFolderFile));
+                        intersectedData = intersect.intersect(FileUtils.readFileToString(originalAFolderFile), FileUtils.readFileToString(originalBFolderFile));
                     } catch (IOException e) {
-                    	LOGGER.error("Rrror during reading CSS files and intersect data",e);
+                        LOGGER.error("Rrror during reading CSS files and intersect data", e);
                     }
-                    
+
                     try {
                         FileUtils.write(intersectedFolderFile, intersectedData.get(IntersectDataType.INTERSECTED));
                     } catch (IOException e) {
-                    	LOGGER.error("Can't write intersected file [" + intersectedFolderFile.getAbsolutePath() + "]", e);
+                        LOGGER.error("Can't write intersected file [" + intersectedFolderFile.getAbsolutePath() + "]", e);
                     }
                     try {
                         FileUtils.write(differencesAFolderFile, intersectedData.get(IntersectDataType.DIFFERENCE_A));
                     } catch (IOException e) {
-                    	LOGGER.error("Can't write differences A file [" + differencesAFolderFile.getAbsolutePath() + "]", e);
+                        LOGGER.error("Can't write differences A file [" + differencesAFolderFile.getAbsolutePath() + "]", e);
                     }
                     try {
                         FileUtils.write(differencesBFolderFile, intersectedData.get(IntersectDataType.DIFFERENCE_B));
                     } catch (IOException e) {
-                    	LOGGER.error("Can't write differences B file [" + differencesBFolderFile.getAbsolutePath() + "]", e);
+                        LOGGER.error("Can't write differences B file [" + differencesBFolderFile.getAbsolutePath() + "]", e);
                     }
                 }
             }
 
         }
-	}
-	
-	private static void printInfo() {
-		LOGGER.info("Please provide path to the folder with proper structure as an argument.");
-		LOGGER.info("Folder structure:");
-		LOGGER.info("./oryginals_a");
-		LOGGER.info("./originals_b");
-		LOGGER.info("./intersected");
-		LOGGER.info("./differences_a");
-		LOGGER.info("./differences_b");
+    }
+
+    private static void printInfo() {
+        LOGGER.info("Please provide path to the folder with proper structure as an argument.");
+        LOGGER.info("Folder structure:");
+        LOGGER.info("./oryginals_a");
+        LOGGER.info("./originals_b");
+        LOGGER.info("./intersected");
+        LOGGER.info("./differences_a");
+        LOGGER.info("./differences_b");
     }
 
 }
